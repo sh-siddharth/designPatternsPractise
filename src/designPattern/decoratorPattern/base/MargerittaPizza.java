59dd6974295f1f48e68f1ff1a991d83647957c75
sh-siddharth <siddharth.hota93@gmail.com>
2024-12-10 23:25:50 +0530

    decorator pattern

---
+++ src/designPattern/decoratorPattern/base/MargerittaPizza.java
@@ -0,0 +1,8 @@
+package src.designPattern.decoratorPattern.base;
+
+public class MargerittaPizza extends BasePizza{
+    @Override
+    public int cost() {
+        return 100;
+    }
+}
---
+++ src/designPattern/decoratorPattern/feature/Topplings.java
@@ -0,0 +1,6 @@
+package src.designPattern.decoratorPattern.feature;
+
+import src.designPattern.decoratorPattern.base.BasePizza;
+
+public abstract class Topplings extends BasePizza {
+}
