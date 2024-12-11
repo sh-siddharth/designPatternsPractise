1b98b8499f9c13c9f53320848557726bfa2a33b8
sh-siddharth <siddharth.hota93@gmail.com>
2024-12-12 01:36:19 +0530

    chain of responsibility pattern

---
+++ src/designPattern/chainOfResponsibility/LoggerProcessor.java
@@ -0,0 +1,20 @@
+package src.designPattern.chainOfResponsibility;
+
+public abstract class LoggerProcessor {
+
+    public static int INFO=1;
+    public static int DEBUG=2;
+    public static int ERROR=3;
+
+    LoggerProcessor nextLoggerProcessor;
+
+    public LoggerProcessor(LoggerProcessor loggerProcessor){
+        this.nextLoggerProcessor = loggerProcessor;
+    }
+
+    public void log(int logLevel, String message){
+        if(nextLoggerProcessor != null){
+            nextLoggerProcessor.log(logLevel, message);
+        }
+    }
+}
